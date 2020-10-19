9aead317c1659f93b5ccfda9cdca1a8dabcec001
Balaks20Dev <Balaks0702@outlook.com>
2020-10-18 22:45:33 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace F20_BookClub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
