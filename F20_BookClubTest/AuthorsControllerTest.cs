d3087f7a51e9e43bb97660c7c3ddfdf975b38db1
Balaks20Dev <Balaks0702@outlook.com>
2020-12-06 12:26:24 -0500

    test I

---
+++ F20_BookClubTest/AuthorsControllerTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace F20_BookClubTest
+{
+    [TestClass]
+    public class AuthorsControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
